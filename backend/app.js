e59790d6da3cee161415213ab11651df3e1493a2
EddieTanDJ <2002226@sit.singaporetech.edu.sg>
2022-05-22 13:16:15 +0800

    Add more API call for the hangman

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,16 @@
+const fetch = require('node-fetch');
 const express = require('express')
 const app = express()
-const router = express.Router();
 const cors = require('cors')
 const port = 3001
+
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/helloworld', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.get('/programming', (req, res) => {
+// Get a word from programming category
+app.get('/programming%20language', (req, res) => {
     const PROGRAMING_LANG = [
         "agda",
         "basic",
@@ -65,9 +64,287 @@ app.get('/programming', (req, res) => {
       function programming() {
         return PROGRAMING_LANG[Math.floor(Math.random() * PROGRAMING_LANG.length)];
       }
+      // Uncomment for answer
       const word = programming();
       console.log(word);
       res.send(word);
+})
+
+// Get a word from food category
+app.get('/food', (req, res) => {
+  const FOOD = [
+      "chicken",
+      "beef",
+      "pork",
+      "fish",
+      "vegetables",
+      "fruits",
+      "dairy",
+      "eggs",
+      "bread",
+      "nuts",
+      "cereals",
+      "sweets",
+      "noodles",
+      "pasta",
+      "soup",
+      "rice",
+      "potatoes",
+      "yogurt",
+      "cake",
+      "fries",
+      "breads",
+      "chips",
+      "sausages",
+      "cheese",
+      "mushrooms",
+      "chocolate",
+      "juice",
+      "soda",
+      "hamburger",
+      "sandwich",
+      "pizza",
+      "pastries",
+      "salad",
+      "sushi",
+      "steak",
+      "taco",
+      "tortilla",
+      "burrito",
+      "waffles",
+      "french fries",
+      "macaroni",
+      "pancakes",
+      "crackers"
+    ];
+
+    function food() {
+      return FOOD[Math.floor(Math.random() * FOOD.length)];
+    }
+    const word = food();
+    // Uncomment for answer
+    console.log(word);
+    res.send(word);
+})
+
+// Get a word from food category
+app.get('/countries', (req, res) => {
+  const COUNTRIES = [
+      "afghanistan",
+      "albania",
+      "algeria",
+      "andorra",
+      "angola",
+      "argentina",
+      "armenia",
+      "australia",
+      "austria",
+      "azerbaijan",
+      "bahamas",
+      "bahrain",
+      "bangladesh",
+      "barbados",
+      "belarus",
+      "belgium",
+      "belize",
+      "benin",
+      "bhutan",
+      "bolivia",
+      "botswana",
+      "brazil",
+      "brunei",
+      "bulgaria",
+      "burundi",
+      "cambodia",
+      "cameroon",
+      "canada",
+      "chad",
+      "chile",
+      "china",
+      "colombia",
+      "comoros",
+      "congo",
+      "croatia",
+      "cuba",
+      "cyprus",
+      "denmark",
+      "djibouti",
+      "dominica",
+      "ecuador",
+      "egypt",
+      "eritrea",
+      "estonia",
+      "ethiopia",
+      "fiji",
+      "finland",
+      "france",
+      "gabon",
+      "gambia",
+      "georgia",
+      "germany",
+      "ghana",
+      "greece",
+      "grenada",
+      "guatemala",
+      "guinea",
+      "guyana",
+      "haiti",
+      "honduras",
+      "hungary",
+      "iceland",
+      "india",
+      "indonesia",
+      "iran",
+      "iraq",
+      "ireland",
+      "isreal",
+      "italy",
+      "jamaica",
+      "japan",
+      "jordan",
+      "kazakhstan",
+      "kenya",
+      "kiribati",
+      "north korea",
+      "south korea",
+      "kosovo",
+      "kuwait",
+      "kyrgyzstan",
+      "laos",
+      "latvia",
+      "lebanon",
+      "lesotho",
+      "liberia",
+      "libya",
+      "liechtenstein",
+      "lithuania",
+      "luxembourg",
+      "macedonia",
+      "madagascar",
+      "malawi",
+      "malaysia",
+      "maldives",
+      "mali",
+      "malta",
+      "mauritania",
+      "mauritius",
+      "mexico",
+      "micronesia",
+      "moldova",
+      "monaco",
+      "mongolia",
+      "montenegro",
+      "morocco",
+      "mozambique",
+      "myanmar",
+      "namibia",
+      "nauru",
+      "nepal",
+      "netherlands",
+      "nicaragua",
+      "niger",
+      "nigeria",
+      "norway",
+      "oman",
+      "pakistan",
+      "palau",
+      "panama",
+      "paraguay",
+      "peru",
+      "philippines",
+      "poland",
+      "portugal",
+      "qatar",
+      "romania",
+      "russia",
+      "rwanda",
+      "samoa",
+      "senegal",
+      "serbia",
+      "seychelles",
+      "sierra leone",
+      "singapore",
+      "slovakia",
+      "slovenia",
+      "somalia",
+      "spain",
+      "sudan",
+      "suriname",
+      "swaziland",
+      "sweden",
+      "switzerland",
+      "syria",
+      "taiwan",
+      "tajikistan",
+      "tanzania",
+      "thailand",
+      "togo",
+      "tonga",
+      "tunisia",
+      "turkey",
+      "turkmenistan",
+      "tuvalu",
+      "uganda",
+      "ukraine",
+      "uruguay",
+      "uzbekistan",
+      "vanuatu",
+      "venezuela",
+      "vietnam",
+      "yemen",
+      "zambia",
+      "zimbabwe",
+      "bosnia and herzegovina",
+      "central african republic",
+      "democratic Republic of the congo",
+      "dominican Republic",
+      "equatorial Guinea",
+      "marshall Islands",
+      "papua New Guinea",
+      "saint Vincent and the Grenadines",
+      "sierra Leone",
+      "saint Kitts and Nevis",
+      "saint Lucia",
+      "san Marino",
+      "saint tome and Principe",
+      "saudi arabia",
+      "sierra Leone",
+      "solmon Island",
+      "south africa",
+      "south sudan",
+      "sri Lanka",
+      "trinidad and tobago",
+      "united arab Emirates",
+      "united Kingdom",
+      "united states of america",
+      "new zealand"
+    ];
+
+    function countries() {
+      return COUNTRIES[Math.floor(Math.random() * COUNTRIES.length)];
+    }
+    const word = countries();
+    // Uncomment for answer
+    console.log(word);
+    res.send(word);
+})
+
+// Get a random word from the list
+app.get('/word', (req, res) => {
+    fetch('https://random-words-api.vercel.app/word')
+    .then(res => res.json())
+    .then(json => {
+      // console.log(json);
+      // Get the word from the response
+      var word = json[0].word;
+      word = word.toLowerCase();
+      // Uncomment for answer
+      console.log(word)
+      res.send(word);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 })
 
 app.listen(port, () => {
--- frontend/src/components/Hangman.js
+++ frontend/src/components/Hangman.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 import step0 from './images/0.jpg';
@@ -11,6 +11,7 @@ import step6 from './images/6.jpg';
 
 let gameStat;
 let score = 0;
+let categories = ['programming language', 'food', 'countries', 'word'];
 
 class Hangman extends Component {
 	static defaultProps = {
@@ -23,13 +24,14 @@ class Hangman extends Component {
 		this.state = {
 			mistake: 0,
 			guessed: new Set(),
-			answer: " ",
+			answer: ' ',
+			category: '',
 		};
 		this.handleGuess = this.handleGuess.bind(this);
 		this.keyPress = this.keyPress.bind(this);
 		window.addEventListener('keydown', this.keyPress);
-    this.nextButton = this.nextButton.bind(this);
-    this.resetButton = this.resetButton.bind(this);
+		this.nextButton = this.nextButton.bind(this);
+		this.resetButton = this.resetButton.bind(this);
 	}
 
 	guessedWord() {
@@ -37,14 +39,18 @@ class Hangman extends Component {
 			.split('')
 			.map((bingo) => (this.state.guessed.has(bingo) ? bingo : '_'));
 	}
-  // TODO: Add more categories of the hangman
-	componentWillMount() {
+	// TODO: Add more categories of the hangman
+	componentDidMount() {
+		let category = this.getCategory();
+		console.log(category)
 		axios
-			.get('/programming')
+			.get('/' + category)
 			.then((res) => {
-				console.log(res.data);
+				// Uncomment to see the answer
+				// console.log(res.data);
 				this.setState({
 					answer: res.data,
+					category: category,
 				});
 			})
 			.catch((err) => {
@@ -70,7 +76,15 @@ class Hangman extends Component {
 		 * 97 = a (Small)
 		 * 122 = z (Small)
 		 */
-		if (gameStat === 'YOU WON' || gameStat === 'YOU LOST') {
+		if (gameStat === 'YOU WON') {
+			if (
+				event.keyCode === 8 ||
+				event.keyCode === 13 ||
+				event.keyCode === 32
+			) {
+				this.nextButton();
+			}
+		} else if (gameStat === 'YOU LOST') {
 			if (
 				event.keyCode === 8 ||
 				event.keyCode === 13 ||
@@ -78,22 +92,18 @@ class Hangman extends Component {
 			) {
 				this.resetButton();
 			}
-		} else if (
-			(event.keyCode >= 65 && event.keyCode <= 90) ||
-			(event.keyCode >= 97 && event.keyCode <= 122)
-		) {
+		} else if (event.keyCode >= 65 && event.keyCode <= 90) {
+			// Convert to lowercase from upper case
+			var letter = String.fromCharCode(event.keyCode).toLowerCase();
+			this.handleGuess(letter);
+		} else if (event.keyCode >= 97 && event.keyCode <= 122 || event.keyCode === 32) {
 			this.handleGuess(event.key);
-		} else if (
-			event.keyCode === 8 ||
-			event.keyCode === 13 ||
-			event.keyCode === 32
-		) {
-			this.resetButton();
-		} else {
+		}
+		else {
 		}
 	}
 	generateButtons() {
-		return 'abcdefghijklmnopqrstuvwxyz'.split('').map((letter) => (
+		return 'abcdefghijklmnopqrstuvwxyz '.split('').map((letter) => (
 			<button
 				key={letter}
 				value={letter}
@@ -106,114 +116,126 @@ class Hangman extends Component {
 	}
 
 	resetButton = () => {
-    axios.get('/programming')
-    .then((res) => {
-      console.log(res.data);
-      this.setState({
-        mistake: 0,
-        guessed: new Set(),
-        answer: res.data,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-    score = 0;
+		let category = this.getCategory();
+		console.log(category)
+		axios
+			.get('/' + category)
+			.then((res) => {
+				// Uncomment to see the answer
+				// console.log(res.data);
+				this.setState({
+					mistake: 0,
+					guessed: new Set(),
+					answer: res.data,
+					category: category,
+				});
+				console.log(this.state.answer);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		score = 0;
 		console.log(score);
-		console.log(this.state.answer);
 	};
 
 	nextButton = () => {
-    axios.get('/programming')
-    .then((res) => {
-      console.log(res.data);
-      this.setState({
-        mistake: 0,
-        guessed: new Set(),
-        answer: res.data,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+		let category = this.getCategory();
+		console.log(category)
+		axios
+			.get('/' + category)
+			.then((res) => {
+				// Comment off to see the answer
+				// console.log(res.data);
+				this.setState({
+					mistake: 0,
+					guessed: new Set(),
+					answer: res.data,
+					category: category,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 		console.log(score);
-		console.log(this.state.answer);
 	};
 
+	getCategory() {
+		// Generate Random Number from 0-3
+		let randomNumber = Math.floor(Math.random() * categories.length);
+		return categories[randomNumber];
+	}
 
 	render() {
-    const { mistake, answer } = this.state;
-    const { maxWrong, images } = this.props;
-    const gameOver = mistake >= maxWrong;
-    const altText = `${mistake}/${maxWrong} wrong guesses`;
-    let Button;
-    const isWinner = this.guessedWord().join('') === answer;
-    console.log(this.state.answer);
-    gameStat = this.generateButtons();
-    if (isWinner) {
-      score++;
-      // Set the score
-      console.log(score);
-      gameStat = 'YOU WON';
-    }
-  if (gameOver) {
-    gameStat = 'YOU LOST';
-  }
-  if (gameStat === 'YOU WON') {
-    Button = (
-      <div>
-        <p className="text-center text-warning mt-6">
-          SCORE: {score}
-        </p>
-        <p className="text-center">
-          <button
-            className="Hangman-reset"
-            onClick={this.nextButton}
-          >
-            Next
-          </button>
-        </p>
-      </div>
-    );
-    }
-    if (gameStat === 'YOU LOST') {
-      Button = (
-        <div>
-          <p className="text-center text-warning mt-6">GAMEOVER!</p>
-          <p className="text-center text-warning mt-6">
-            SCORE: {score}
-          </p>
-          <p className="text-center">
-            <button
-              className="Hangman-reset"
-              onClick={this.resetButton}
-            >
-              Restart
-            </button>
-          </p>
-        </div>
-      );
-  }
-    return (
+		const { mistake, answer } = this.state;
+		const { maxWrong, images } = this.props;
+		const gameOver = mistake >= maxWrong;
+		const altText = `${mistake}/${maxWrong} wrong guesses`;
+		let Button;
+		let categoryQuestion = this.state.category
+		const isWinner = this.guessedWord().join('') === answer;
+		gameStat = this.generateButtons();
+		if (isWinner) {
+			score++;
+			// Set the score
+			console.log(score);
+			gameStat = 'YOU WON';
+		}
+		if (gameOver) {
+			gameStat = 'YOU LOST';
+		}
+		if (gameStat === 'YOU WON') {
+			Button = (
+				<div>
+					<p className="text-center text-warning mt-6">
+						SCORE: {score}
+					</p>
+					<p className="text-center">
+						<button
+							className="Hangman-reset"
+							onClick={this.nextButton}
+						>
+							Next
+						</button>
+					</p>
+				</div>
+			);
+		}
+		if (gameStat === 'YOU LOST') {
+			Button = (
+				<div>
+					<p className="text-center text-warning mt-6">GAMEOVER!</p>
+					<p className="text-center text-warning mt-6">
+						SCORE: {score}
+					</p>
+					<p className="text-center">
+						<button
+							className="Hangman-reset"
+							onClick={this.resetButton}
+						>
+							Restart
+						</button>
+					</p>
+				</div>
+			);
+		}
+		return (
 			<div className="Hangman">
-					<div className="row">
-						<div className="col-sm-5">
-							<a className="text-light" href="/">
-								Hangman
-							</a>
-						</div>
-						<div className="col-sm-4 text-primary">
-							Score : {score}
-						</div>
-						<div className="col-sm-3 text-primary">
-							Guessed wrong: {mistake}
-						</div>
+				<div className="row">
+					<div className="col-sm-5">
+						<a className="text-light" href="/">
+							Hangman
+						</a>
+					</div>
+					<div className="col-sm-4 text-primary">Score : {score}</div>
+					<div className="col-sm-3 text-primary">
+						Wrong guessed: {mistake}
+					</div>
 				</div>
 				<p className="text-center">
 					<img src={images[mistake]} alt={altText} />
 				</p>
 				<p className="text-center text-light">
-					Guess the Programming Language ?
+					Guess the {categoryQuestion}?
 				</p>
 				<p className="Hangman-word text-center">
 					{!gameOver ? this.guessedWord() : answer}{' '}
